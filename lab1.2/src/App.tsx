3b2c80b6bb3668d543fd1d25777307852235f8a2
sania parvej <sparvej@rrc.ca>
2026-09-23 00:40:43 -0500

    Add React foundation for Lab 2.2

---
+++ lab1.2/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import EmployeeDirectory from "./components/employeeDirectory/EmployeeDirectory";
+
+function App() {
+    return (
+        <>
+            <Header />
+
+            <EmployeeDirectory />
+
+            
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ lab1.2/src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header>
+            <img
+                src="https://itsm-ace.ca/images/logo.svg"
+                alt="Pixell River Financial logo"
+            />
+
+            <div>
+                <h1>Pixell River Employee Directory</h1>
+                <p>Welcome to the Pixell River Financial Employee Directory.</p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
