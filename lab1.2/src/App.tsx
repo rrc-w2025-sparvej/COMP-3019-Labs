0d08a18c54fcba41f008f1109044d9ec709fb487
sania parvej <sparvej@rrc.ca>
2026-09-23 00:48:49 -0500

    Add React Router routes

--- lab1.2/src/App.tsx
+++ lab1.2/src/App.tsx
@@ -2,18 +2,22 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import EmployeeDirectory from "./components/employeeDirectory/EmployeeDirectory";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <>
-            <Header />
+  return (
+    <BrowserRouter>
+      <Header />
 
-            <EmployeeDirectory />
+      <Routes>
+        <Route path="/employees" element={<EmployeeDirectory />} />
+        <Route path="/organization" element={<div>Organization</div>} />
+        <Route path="/" element={<EmployeeDirectory />} />
+      </Routes>
 
-            
-            <Footer />
-        </>
-    );
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
